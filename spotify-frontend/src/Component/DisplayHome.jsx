4a40da799f308d31ca296d1d3765bade833514b3
CodedSedorf <sedorfdlc@gmail.com>
2025-05-19 03:56:08 +0100

    second commit

---
+++ spotify-frontend/src/Component/DisplayHome.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const DisplayHome = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="p-5 font-bold text-2xl">
+        <p>56:00</p>
+      </div>
+    </>
+  );
+};
+
+export default DisplayHome;
